e62667f3bb2ff2676003630cda8061d03dbf568d
Brian Floersch <brian.floersch@datadoghq.com>
2022-12-01 17:05:40 -0500

    simplify sed expressions

--- 1/part2.sh
+++ 1/part2.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat 1.txt | tr '\n' '+' | sed 's/++/\n/g' | sed 's/\(.*\)/\1\n/g' | bc | sort -n -r | head -n 3 | tr '\n' '+' | sed 's/\(.*\)+/\1\n/' | bc
+cat 1.txt | tr '\n' '+' | sed 's/++/\n/g; s/\(.*\)/\1\n/g' | bc | sort -n -r | head -n 3 | tr '\n' '+' | sed 's/\(.*\)+/\1\n/' | bc
