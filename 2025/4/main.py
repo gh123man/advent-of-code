fcdc39a3680f5dd0ce9a3aa19db3b496e2b3a556
Brian Floersch <gh123man@gmail.com>
2025-12-04 00:27:05 -0500

    day 4

---
+++ 2025/4/main.py
@@ -0,0 +1,30 @@
+#!/opt/homebrew/bin/python3
+
+from lib import *
+
+f = open('input.txt', 'r')
+grid = Grid(f.read().splitlines())
+
+def countUnderFour(y, x):
+    if grid.get((y, x)) != "@":
+        return False
+    return len([x for _, x, _ in grid.neighbors((y, x), all_edges) if x != None and x != "."]) < 4
+
+part1 = 0
+for y, x in grid.enumerate():
+    if countUnderFour(y, x):
+        part1 += 1
+
+part2 = 0
+while True:
+    removed = False
+    for y, x in grid.enumerate():
+        if countUnderFour(y, x):
+            grid.set((y, x), ".")
+            removed = True
+            part2 += 1
+    if not removed:
+        break
+
+print(part1)
+print(part2)
