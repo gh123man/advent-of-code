08d0f9e1d86b2a0b43d37634c95f14bc69fa9274
Brian Floersch <gh123man@gmail.com>
2025-12-05 08:51:52 -0500

    day 5

---
+++ 2025/5/main.py
@@ -0,0 +1,46 @@
+#!/opt/homebrew/bin/python3
+
+from lib import *
+
+f = open('input.txt', 'r')
+lines = f.read().splitlines()
+
+ranges = []
+items = []
+fillItems = False
+for line in lines:
+    if line == "":
+        fillItems = True
+        continue
+    if fillItems:
+        items.append(int(line))
+        continue
+    ranges.append(range(int(line.split("-")[0]), int(line.split("-")[1]) + 1))
+
+part1 = 0
+for item in items:
+    for r in ranges:
+        if item in r:
+            part1 += 1
+            break
+
+part2 = set()
+
+compressedRanges = []
+ranges.sort(key=lambda x: x.start)
+
+while len(ranges) > 0:
+    curRange = ranges[0]
+    ranges.pop(0)
+    toRemove = []
+    for i, r in enumerate(ranges):
+        if r.start in curRange:
+            curRange = range(curRange.start, max(curRange.stop, r.stop))
+            toRemove.append(i)
+            print(toRemove, i)
+    for i in toRemove[::-1]:
+        ranges.pop(i)
+    compressedRanges.append(curRange)
+
+print(compressedRanges)
+print(sum([len(r) for r in compressedRanges]))
